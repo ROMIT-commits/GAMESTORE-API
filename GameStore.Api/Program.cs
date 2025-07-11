8a6c5f18c24d49a4581e75e85b9a716a344e8ae3
ROMIT-commits <romit6193@sigilotech.com>
2025-07-11 17:41:44 +0530

    Optimised CRUD operation and add endpoints

--- GameStore.Api/Program.cs
+++ GameStore.Api/Program.cs
@@ -1,75 +1,7 @@
-using GameStore.Api.DTOs;
 
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
-const string GetGameEndpointName = "GetGame";
-
-List<GameDTO> games = 
-[
-    new (1, "Game One", "Action", 29.99m, new DateOnly(2023, 1, 15)),
-    new (2, "Game Two", "Adventure", 49.99m, new DateOnly(2023, 2, 20)),
-    new (3, "Game Three", "RPG", 39.99m, new DateOnly(2023, 3, 10))
-];
-
-
-//GET /games
-app.MapGet("games", () => games);
-
-//GET /games/1
-app.MapGet("games/{id}", (int id) =>
-{
-    var game = games.Find(g => g.ID == id);
-    return game is not null ? Results.Ok(game) : Results.NotFound();
-}).WithName(GetGameEndpointName);
-
-
-//POST /games
-
-app.MapPost("games", (CreateGameDTO newGame) =>
-{
-    GameDTO game = new(
-        games.Count + 1,
-        newGame.Name,
-        newGame.Genre,
-        newGame.Price,
-        newGame.ReleaseDate // or RealiseDate if you intended the spelling
-    );
-
-    games.Add(game);
-
-    return Results.CreatedAtRoute(
-        GetGameEndpointName,
-        new { id = game.ID },
-        game
-    );
-});
-
-
-//PUT /games
-
-app.MapPut("games/{id}", (int id, UpdateGameDTO updatedGame) =>
-{
-    var index = games.FindIndex(g => g.ID == id);
-    
-    // ✅ Always check if index is valid
-    if (index == -1)
-    {
-        return Results.NotFound();
-    }
-
-    games[index] = new GameDTO(
-        id,
-        updatedGame.Name,
-        updatedGame.Genre,
-        updatedGame.Price,
-        updatedGame.ReleaseDate
-    );
-
-    return Results.NoContent();
-});
-
-
-
+app.MapGamesEndpoints();
 
 app.Run();
--- GameStore.Api/obj/Debug/net9.0/GameStore.Api.AssemblyInfo.cs
+++ GameStore.Api/obj/Debug/net9.0/GameStore.Api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("GameStore.Api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f4fbea44b91fad9a30cfdf1dfe2c79522f991f83")]
 [assembly: System.Reflection.AssemblyProductAttribute("GameStore.Api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("GameStore.Api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
